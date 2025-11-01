ca2a056a90f0af2adc92414c7c98f492ec65f64e
darkstarworks <darkstarworks@gmail.com>
2025-11-01 09:02:05 +0800

    first commit

---
+++ src/main/kotlin/io/github/darkstarworks/PluginGuard.kt
@@ -0,0 +1,14 @@
+package io.github.darkstarworks
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class pluginGuard : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
